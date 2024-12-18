0c08a4dc5fa3dd9607d04630e568a03485818a25
Ajay K <ajaykfan@gmail.com>
2024-12-19 00:06:01 +0530

    feat: add header and footer

---
+++ src/components/Header/menuData.jsx
@@ -0,0 +1,58 @@
+const menuData = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Objectives",
+    path: "/objectives",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Guidelines",
+    path: "/guidelines",
+    newTab: false,
+  },
+  {
+    id: 4,
+    title: "Resources",
+    path: "/resources",
+    newTab: false,
+  },
+  {
+    id: 5,
+    title: "Courses at IITH",
+    newTab: false,
+    submenu: [
+      {
+        id: 51,
+        title: "2020",
+        path: "/courses/2020",
+        newTab: false,
+      },
+      {
+        id: 52,
+        title: "2017-2019",
+        path: "/courses/2017-2019",
+        newTab: false,
+      },
+      {
+        id: 53,
+        title: "2014-2016",
+        path: "/courses/2015-2016",
+        newTab: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Contact",
+    path: "/contact",
+    newTab: false,
+  },
+];
+export default menuData;
