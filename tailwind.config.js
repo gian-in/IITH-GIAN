c85b359b7d7919a65cf8f2ecd33282ad3dd5011c
Ajay K <ajaykfan@gmail.com>
2024-12-19 14:03:22 +0530

    feat: add NextUI and Framer Motion dependencies with Tailwind CSS integration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
 const colors = require("tailwindcss/colors");
+const { nextui } = require("@nextui-org/react");
 
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   darkMode: "class",
   theme: {
@@ -77,5 +79,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    nextui()
+  ],
 };
