e421ce0083b742b226f0497164db016e06fb5df4
Ajay K <ajaykfan@gmail.com>
2024-12-28 16:50:55 +0530

    feat: update course routing to include new phases and remove outdated course paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
-import Courses2020 from "./pages/courses/Courses2020";
-import Courses2015 from "./pages/courses/Courses2015";
-import Courses2017 from "./pages/courses/Courses2017";
+import CoursesPhase3 from "./pages/courses/CoursesPhase3";
+import CoursesPhase1 from "./pages/courses/CoursesPhase1";
+import CoursesPhase2 from "./pages/courses/CoursesPhase2";
 import Contact from "./pages/contact/Contact";
 import Objectives from "./pages/objectives/Objectives";
 import Guidelines from "./pages/guidelines/Guidelines";
 import ErrorPage from "./pages/error/page";
+import CoursesPhase4 from "./pages/courses/CoursesPhase4";
 
 const App = () => {
   return (
@@ -17,10 +18,11 @@ const App = () => {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/courses" element={<Navigate to="/courses/2020" />} />
-          <Route path="/courses/2020" element={<Courses2020 />} />
-          <Route path="/courses/2017-2019" element={<Courses2017 />} />
-          <Route path="/courses/2015-2016" element={<Courses2015 />} />
+          <Route path="/courses" element={<Navigate to="/courses/phase4" />} />
+          <Route path="/courses/phase4" element={<CoursesPhase4 />} />
+          <Route path="/courses/phase3" element={<CoursesPhase3 />} />
+          <Route path="/courses/phase2" element={<CoursesPhase2 />} />
+          <Route path="/courses/phase1" element={<CoursesPhase1 />} />
           <Route path="/objectives" element={<Objectives />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/guidelines-forms" element={<Guidelines />} />
