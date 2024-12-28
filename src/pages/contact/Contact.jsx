b76fd55c4106de7c925089d097afd1a9cbf6649d
Ajay K <ajaykfan@gmail.com>
2024-12-28 17:44:14 +0530

    feat: improve layout of Local Coordinator section in Contact component

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -16,13 +16,13 @@ const Contact = () => {
             <div className="w-full px-4 grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
               <div id="contact-local-coordinator" className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]">
                 <h3 className="mb-12 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">Local Coordinator</h3>
-                <div className="flex items-center space-x-6 lg:space-x-12">
+                <div className="flex flex-col items-start space-x-0 lg:items-center lg:space-x-12 lg:flex-row">
                   <img
                     src="assets/anil.png"
                     alt="teacher"
                     className="w-32 h-32 md:w-40 md:h-40 lg:w-48 lg:h-48 rounded"
                   />
-                  <div className="space-y-1">
+                  <div className="space-y-1 mt-4 lg:mt-0">
                     <h4 className="text-xl font-bold">Dr. Anil Agarwal</h4>
                     <p className="text-base">Local Coordinator</p>
                     <p className="text-base pt-2">Associate Professor</p>
